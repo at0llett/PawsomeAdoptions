12a0e44d3cddf036b6d4a0cfdc2b872d1edfe787
Tammy Nguyen <tnguyen9448@gmail.com>
2024-03-06 16:32:22 -0800

    contact

---
+++ pawsomeadoption/src/frontend/Contact.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import emailjs from '@emailjs/browser';
+
+const Contact = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Your EmailJS service ID, template ID, and Public Key
+    const serviceId = 'service_xgh5pzc';
+    const templateId = 'template_kz7i84f';
+    const publicKey = '8BnRXD3GSfclErmkE';
+
+    // Create a new object that contains dynamic template params
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Web Wizard',
+      message: message,
+    };
+
+    // Send the email using EmailJS
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        console.log('Email sent successfully!', response);
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.error('Error sending email:', error);
+      });
+  }
+    return (
+        <form onSubmit={handleSubmit} className='emailForm'>
+          <input
+            type="text"
+          
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            cols="30"
+            rows="10"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          >
+          </textarea>
+          <button type="submit">Send Email</button>
+        </form>
+      )
+    }
+export default Contact
